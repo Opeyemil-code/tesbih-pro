f8d20e0a44746796ef04fd95df6d9a1607cc7e9b
Opeyemil-code <opeyemitoheeb31@gmail.com>
2022-09-26 09:37:38 -1200

    first work

--- script.js
+++ script.js
@@ -18,7 +18,9 @@ const divBody = document.querySelector('.subbody')
 const savedDhikr = document.querySelector('.saved')
 const bgchange = document.querySelectorAll('.bgchange')
 const bgLightChange = document.querySelectorAll('.light')
-
+const numCount = document.querySelector('.numcount')
+const counterBtn = document.querySelector('.presscount')
+const deact = document.querySelector('.deactivate')
 
 
 //THIS IS FOR THE MENU
@@ -42,6 +44,10 @@ for (let i = 0; i < nightModeBtn.length; i++) {
 }
 
 
+//THIS IS MEANT FOR CHANGES BETWEEN NIGHT MODE AND OTHERS
+
+
+
 
 //THIS IS FOR CHANGING DIFFERENT BACKGROUND COLOR
 const diffColors = [
@@ -55,42 +61,65 @@ const lightColors =[
 
 let numCounter = 0
 
+function backGroundChange() {
+    if (numCounter <= diffColors.length+1) {
+        document.querySelector('.subbody').style.backgroundColor = diffColors[numCounter];
+        document.querySelector('.bigcircle').style.backgroundColor  = diffColors[numCounter];
+        document.querySelector('.tab').style.backgroundColor  = diffColors[numCounter];
+        document.querySelector('.sdhikr').style.backgroundColor  = diffColors[numCounter];
+        document.querySelector('.counter').style.backgroundColor  = diffColors[numCounter];
+        document.querySelector('.calcnumber').style.backgroundColor  = 'white';
+        document.querySelector('.saved').style.backgroundColor = diffColors[numCounter]
+
+        for (let i = 0; i < bgchange.length; i++) {
+            bgchange[i].style.backgroundColor = diffColors[numCounter]
+            
+        }
+
+        for (let i = 0; i < bgLightChange.length; i++) {
+            bgLightChange[i].style.backgroundColor = lightColors[numCounter];
+            
+        }
+       
+
+    } 
+
+    numCounter++
+}
+
 for (let i = 0; i < changeColorbtn.length; i++) {
     changeColorbtn[i].addEventListener(
-        'click', function () {
-            if (numCounter <= diffColors.length+1) {
-                document.querySelector('.subbody').style.backgroundColor = diffColors[numCounter];
-                document.querySelector('.bigcircle').style.backgroundColor  = diffColors[numCounter];
-                document.querySelector('.tab').style.backgroundColor  = diffColors[numCounter];
-                document.querySelector('.sdhikr').style.backgroundColor  = diffColors[numCounter];
-                document.querySelector('.counter').style.backgroundColor  = diffColors[numCounter];
-                document.querySelector('.calcnumber').style.backgroundColor  = 'white';
-                document.querySelector('.saved').style.backgroundColor = diffColors[numCounter]
-
-                for (let i = 0; i < bgchange.length; i++) {
-                    bgchange[i].style.backgroundColor = diffColors[numCounter]
-                    
-                }
-
-                for (let i = 0; i < bgLightChange.length; i++) {
-                    bgLightChange[i].style.backgroundColor = lightColors[numCounter];
-                    
-                }
-               
-    
-            } 
-    
-            numCounter++
-        }
-    
-
-        
-    )
+        'click', backGroundChange)
     
     }
 
 
 
+
+//FUNCTIONALITY FOR THE COUNTER BUTTON
+
+let count = 0;
+
+function handler(params) {
+    count++
+    document.querySelector('.numcount').textContent = count; 
+}
+
+counterBtn.addEventListener(
+    'click', handler
+)
+
+
+
+function reset(params) {
+     count= 0;
+    document.querySelector('.numcount').textContent = count; 
+}
+
+deact.addEventListener(
+    'click', reset
+    
+)
 
 
 
