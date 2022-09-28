2e5209e906be554e7613490472e46064b204213b
Opeyemil-code <opeyemitoheeb31@gmail.com>
2022-09-27 12:38:35 -1200

    counter

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const bgLightChange = document.querySelectorAll('.light')
 const numCount = document.querySelector('.numcount')
 const counterBtn = document.querySelector('.presscount')
 const deact = document.querySelector('.deactivate')
+const reverseBtn = document.querySelector('.reverseBtn')
 
 
 //THIS IS FOR THE MENU
@@ -33,87 +34,105 @@ function toggleMenu(params) {
 toggleBtn.addEventListener('click', toggleMenu) 
 
 
+
+/***********************************************
+ * *********************************************
+ */
+
 //THIS IS FOR THE NIGHT MODE BUTTON
+function night(params) {
+    body.classList.toggle('dark')
+}
 for (let i = 0; i < nightModeBtn.length; i++) {
     nightModeBtn[i].addEventListener(
-        'click', function name(params) {
-            body.classList.toggle('dark')
-        }
+        'click', night
     )
     
 }
 
 
-//THIS IS MEANT FOR CHANGES BETWEEN NIGHT MODE AND OTHERS
+
+
 
 
 
 
 //THIS IS FOR CHANGING DIFFERENT BACKGROUND COLOR
 
+/***********************************************
+ * *********************************************
+ */
+
+//array for the sub colors
 const lightColors =[
     '#770000','#ffe5e5','#7b7bff', '#ff85ff', '#7dff7d'
 ];
 
+//array for colors needed
 const diffColors = [
     '#470000','#ffbfbf','#4d4dff', 'purple','green'
 ];
 
-let numCounter=0
-console.log(numCounter)
+
+//Sub colors LOOP
+
+
+let numCounter = 0
 function secondaryChange(params) {
+    for (let i = 0; i < bgLightChange.length; i++) {
+        bgLightChange[i].style.backgroundColor = lightColors[numCounter]   
+    }
+
     for (let i = 0; i < bgchange.length; i++) {
         bgchange[i].style.backgroundColor = diffColors[numCounter]
         
     }
-
-    for (let i = 0; i < bgLightChange.length; i++) {
-          bgLightChange[i].style.backgroundColor = lightColors[numCounter]
 }
-  
 
-} 
 
-//NEW FUNCTION
-function setter() {
+//ELEMENT TO BE CHANGED
+
+
+function setter(params) {
     document.querySelector('.subbody').style.backgroundColor = diffColors[numCounter]
-       document.querySelector('.bigcircle').style.backgroundColor  = diffColors[numCounter];
-        document.querySelector('.tab').style.backgroundColor  = diffColors[numCounter];
-        document.querySelector('.sdhikr').style.backgroundColor  = diffColors[numCounter];
-        document.querySelector('.counter').style.backgroundColor  = diffColors[numCounter];
-        document.querySelector('.calcnumber').style.backgroundColor  = 'white';
-        document.querySelector('.saved').style.backgroundColor = diffColors[numCounter] 
-
-        secondaryChange()
+    document.querySelector('.bigcircle').style.backgroundColor  = diffColors[numCounter];
+     document.querySelector('.tab').style.backgroundColor  = diffColors[numCounter];
+     document.querySelector('.sdhikr').style.backgroundColor  = diffColors[numCounter];
+     document.querySelector('.counter').style.backgroundColor  = diffColors[numCounter];
+     document.querySelector('.calcnumber').style.backgroundColor  = 'white';
+     document.querySelector('.saved').style.backgroundColor = diffColors[numCounter]
      
-}   
+     secondaryChange()
+}
 
 
-
+//LOOP FOR ELEMENT TO BE CHANGED
 function backGroundChange() {
     if (numCounter < diffColors.length-1) {
         numCounter++
+        console.log(numCounter)
         setter()
-      
+       
         
     } 
     else{
         
         numCounter = 0
         setter()
-        console.log(numCounter)
+        
     }
 }  
 
-
-
-
+//CLICK EVENT TO MAKE THEM HAPPEN
 for (let i = 0; i < changeColorbtn.length; i++) {
     changeColorbtn[i].addEventListener(
         'click', backGroundChange
     )
     
 }
+
+
+
 
 
 
@@ -142,6 +161,9 @@ for (let i = 0; i < changeColorbtn.length; i++) {
 
 //FUNCTIONALITY FOR THE COUNTER BUTTON
 
+/***********************************************
+ * *********************************************
+ */
 let count = 0;
 
 function handler(params) {
@@ -164,6 +186,22 @@ deact.addEventListener(
     'click', reset
     
 )
+
+
+//FUNCTIONALITY FOR THE REVERSE BUTTON;
+
+function reverse(params) {
+    count--
+    document.querySelector('.numcount').textContent = count
+    
+ }
+
+
+reverseBtn.addEventListener(
+    'click', reverse
+)
+
+
 
 
 
